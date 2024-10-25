3e4208f46b3a93ca4cfa80303513ca3f337a63e1
Vikash Thakur <thakvikash622@gmail.com>
2024-10-25 16:35:03 +0530

    Fixed: Bugs

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { heroVideo, smallHeroVideo } from "../utils"
 import { useEffect } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 const Hero = () => {
 
   const [videosrc, SetVideosrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo)
@@ -21,17 +23,33 @@ const Hero = () => {
     }
   },[])
 
-  
+  useGSAP(() => {
+    gsap.to("#hero", {
+      opacity: 1,
+      delay: 1
+    })
+
+    gsap.to("#detail", {
+      y: -20,
+      opacity: 1,
+      delay: 1.4,
+      duration: 1
+    })
+  },[])
 
   return (
     <section className="w-full h-[calc(100vh-80px)] bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
-        <p className="hero-title">Iphone 15 Pro</p>
+        <p id="hero" className="hero-title">Iphone 15 Pro</p>
         <div className="md:w-10/12 w-9/12">
           <video src={videosrc} className="pointer-events-none" autoPlay muted loop>
             <source src={videosrc} type="video/mp4" />
           </video>
         </div>
+      </div>
+      <div id="detail" className="flex flex-col items-center justify-center translate-y-20 opacity-0">
+        <a href="#highlights" className="btn">Buy</a>
+        <p className="font-normal text-xl">From $199/month or $999</p>
       </div>
     </section>
   );
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,28 +1,28 @@
 import { appleImg, bagImg, searchImg } from "../utils"
-import { navLists} from "../constants"
+import { navLists } from "../constants"
 const Navbar = () => {
-  return (
-    <header className="w-full p-5 md:px-10 flex justify-between items-center">
-        <nav className="flex items-center w-full screen-max-width">
-            <div>
-                <img src={appleImg} alt="Apple" width={32} height={32} />
-            </div>
-            <div className="flex flex-1 justify-center max-sm:hidden">
-                {
-                    navLists.map((nav) => (
-                        <div key={nav} className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all">
-                            {nav}
-                        </div>
-                    ))
-                }
-            </div>
-            <div className="flex items-center gap-5 cursor-pointer flex-1 justify-end">
-            <img src={searchImg} alt="Search" width={20} height={20} />
-            <img src={bagImg} alt="Bag" width={20} height={20} />
-            </div>
-        </nav>
-    </header>
-  );
+    return (
+        <header className="w-full p-5 md:px-10 flex justify-between items-center">
+            <nav className="flex items-center w-full screen-max-width">
+                <div>
+                    <img src={appleImg} alt="Apple" width={32} height={32} />
+                </div>
+                <div className="flex flex-1 justify-center max-sm:hidden">
+                    {
+                        navLists.map((nav) => (
+                            <div key={nav} className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all">
+                                {nav}
+                            </div>
+                        ))
+                    }
+                </div>
+                <div className="flex items-center gap-5 cursor-pointer max-sm:flex-1 max-sm:justify-end">
+                    <img src={searchImg} alt="Search" width={20} height={20} />
+                    <img src={bagImg} alt="Bag" width={20} height={20} />
+                </div>
+            </nav>
+        </header>
+    );
 };
 
 export default Navbar;
