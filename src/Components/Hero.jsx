8ca956f976f596f295298cf1b212853487f658f0
Vikash Thakur <thakvikash622@gmail.com>
2024-10-25 16:19:11 +0530

    Fixing: Hero page

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,8 +1,39 @@
+import { useState } from "react";
+import { heroVideo, smallHeroVideo } from "../utils"
+import { useEffect } from "react";
 const Hero = () => {
+
+  const [videosrc, SetVideosrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo)
+
+  const handleVideosize = () => {
+    if(window.innerWidth < 760){
+      SetVideosrc(smallHeroVideo)
+    } else {
+      SetVideosrc(heroVideo)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleVideosize)
+
+    return () => {
+      window.removeEventListener('resize', handleVideosize)
+    }
+  },[])
+
+  
+
   return (
-    <div>
-      <h1>Hero</h1>
-    </div>
+    <section className="w-full h-[calc(100vh-80px)] bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p className="hero-title">Iphone 15 Pro</p>
+        <div className="md:w-10/12 w-9/12">
+          <video src={videosrc} className="pointer-events-none" autoPlay muted loop>
+            <source src={videosrc} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+    </section>
   );
 };
 
